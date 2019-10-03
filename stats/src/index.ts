c6a93854fe6157ab9e9d8f85d3902c7820e82446
Sergey Ursul <serg.ursul@gmail.com>
2019-10-04 01:19:41 +0300

    new project - stats

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hi there');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('./football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+
+enum MatchResults {
+	HomeWin = 'H',
+	AwayWin = 'A',
+	Draw = 'D'
+}
+
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
